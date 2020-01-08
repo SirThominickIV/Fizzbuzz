7985f0f15434f3a212625d365adfcd568d69eeb7
Thomas Snyder <52889852+SirThominickIV@users.noreply.github.com>
2020-01-08 12:53:32 -0500

    Add files via upload

---
+++ Fizzbuzz_Snyder/Fizzbuzz_Snyder/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Fizzbuzz_Snyder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb_Output = new System.Windows.Forms.RichTextBox();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.btn_Next100 = new System.Windows.Forms.Button();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // rtb_Output
+            // 
+            this.rtb_Output.Location = new System.Drawing.Point(64, 24);
+            this.rtb_Output.Name = "rtb_Output";
+            this.rtb_Output.Size = new System.Drawing.Size(219, 385);
+            this.rtb_Output.TabIndex = 0;
+            this.rtb_Output.Text = "";
+            this.rtb_Output.TextChanged += new System.EventHandler(this.rtb_Output_TextChanged);
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Location = new System.Drawing.Point(49, 436);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(75, 23);
+            this.btn_Next.TabIndex = 1;
+            this.btn_Next.Text = "Next";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Next_Click);
+            // 
+            // btn_Next100
+            // 
+            this.btn_Next100.Location = new System.Drawing.Point(130, 436);
+            this.btn_Next100.Name = "btn_Next100";
+            this.btn_Next100.Size = new System.Drawing.Size(75, 23);
+            this.btn_Next100.TabIndex = 2;
+            this.btn_Next100.Text = "Next 100";
+            this.btn_Next100.UseVisualStyleBackColor = true;
+            this.btn_Next100.Click += new System.EventHandler(this.btn_Next100_Click);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Location = new System.Drawing.Point(211, 436);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_Reset.TabIndex = 3;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(244, 485);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(95, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "What is Fizz buzz?";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 507);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.btn_Next100);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.rtb_Output);
+            this.Name = "Form1";
+            this.Text = "Fizz buzz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb_Output;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Button btn_Next100;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Fizzbuzz_Snyder/Source.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Fizzbuzz_Snyder
+{
+    public partial class Form1 : Form
+    {
+        //Variables
+        int Count = 1;                      //The number that is being counted
+        string OutputPlaceholder = "";      //An output placeholder string, it get's built up as more divisors are added
+        List<FizzBuzzDivisor> Divisors = new List<FizzBuzzDivisor>();     //This list of divisors such as "Fizz" and "Buzz"
+
+        public Form1()
+        {
+            //Divisors
+            FizzBuzzDivisor Fizz = new FizzBuzzDivisor("Fizz", 3); Divisors.Add(Fizz);  //Fizz, gets added for numbers divisible by 3
+            FizzBuzzDivisor Buzz = new FizzBuzzDivisor("Buzz", 5); Divisors.Add(Buzz);  //BUzz, gets added for numbers divisible by 5
+            FizzBuzzDivisor Binn = new FizzBuzzDivisor("Binn", 150); Divisors.Add(Binn); //Binn, my own divisor for numbers divisibile by 150
+
+            InitializeComponent();
+        }
+
+        public void NextNum() 
+        {
+            //Calculates what the next number/phrase should be
+            foreach (FizzBuzzDivisor Divisor in Divisors)
+            {
+                if (IsDivisible(Count, Divisor.Divisor)) OutputPlaceholder += Divisor.Name;
+            }
+
+            if (OutputPlaceholder == "") OutputPlaceholder += Count;
+
+            //Sending Output
+            rtb_Output.Text += OutputPlaceholder + System.Environment.NewLine;
+
+            //Reseting and advancing variables
+            Count++;
+            OutputPlaceholder = "";
+        }
+
+        public bool IsDivisible(int Num1, int Num2) //Checks to see if Num1 is divisible by Num2
+        {
+            if ((Num1 % Num2) == 0) return true;
+            return false;
+        }
+
+        private void btn_Next_Click(object sender, EventArgs e) //The next button
+        {
+            NextNum();  
+        }
+
+        private void btn_Next100_Click(object sender, EventArgs e) //Same as the next button, but advances by 100
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                NextNum();
+            }
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e) //Resets the game
+        {
+            Count = 1;
+            rtb_Output.Clear();
+        }
+
+        private void rtb_Output_TextChanged(object sender, EventArgs e) //Auto scrolls the output text box to the bottom
+        {
+            rtb_Output.SelectionStart = rtb_Output.Text.Length;
+            rtb_Output.ScrollToCaret();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) //Hyper link for more information on what Fizz buzz is
+        {
+            Process.Start("https://en.wikipedia.org/wiki/Fizz_buzz");
+        }
+    }
+
+    public class FizzBuzzDivisor {  //The divisor class, has a name and divisor for each FizzBuzzDivisor
+        public string Name;
+        public int Divisor;
+
+        public FizzBuzzDivisor(string input_Name, int input_Divisor)
+        {
+            Name = input_Name;
+            Divisor = input_Divisor;
+        }
+    }
+}
